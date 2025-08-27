4328385445c3926c9702b466181cf658f1050f0f
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-27 16:46:34 -0300

    feat: adiciona o footer | fechamento do dia 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles.css'
 function App() {
   return (
     <>
-      
+      <Header/>
+      <h1>Olá</h1>
+      <Footer/>
     </>
   )
 }
